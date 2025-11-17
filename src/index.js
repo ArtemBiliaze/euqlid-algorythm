211f6911c589c327082a85160ff03c7053a3d89b
Artem Biliaze <artembiliaze.gmail.com>
2025-11-17 13:58:53 +0200

    Update euqlidAlgorythm

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const euqlidAlgorythm = (a, b) => {
     try {
-        if (a <= 0 && b <= 0)
-          throw new RangeError("Числа мають бути більше нуля");
-        if (b === 0) return a;
-        return euqlidAlgorythm(b, a % b);
+         if (!Number.isInteger(a) || !Number.isInteger(b) || a <= 0 || b <= 0) {
+           throw new Error ("Аргументи повинні бути цілими числами, більше за нуль");
+         }
+         while (b !== 0){
+            let temp = b;
+            b = a % b;
+            a = temp;
+         }
+            return a;
     } catch (error) {
-        return error.message
+        return error.message;
     }
 };
 
-console.log(euqlidAlgorythm(0, 0));
+console.log(euqlidAlgorythm(0, 5));
+console.log(euqlidAlgorythm('', 5));
+console.log(euqlidAlgorythm(undefined, 5));
+console.log(euqlidAlgorythm(5.5, 4.2));
 console.log(euqlidAlgorythm(16, 8));
 console.log(euqlidAlgorythm(10, 13));
+console.log(euqlidAlgorythm(5,5));
+
 
